43833718dd3a0dd93727097455cf72b7fa9628a1
Aleksey-Gavrin <darkgarvin@gmail.com>
2023-10-04 00:05:34 +0300

    1 task complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)    {
+        int intNumber = 156000;
+        byte byteNumber = 64;
+        short shortNumber = 25000;
+        long longNumber = 45000125000L;
+        float floatNumber = 5.86f;
+        double doubleNumber = 4.896321478;
+        System.out.println ("Значение переменной intNumber с типом int равно " + intNumber);
+        System.out.println ("Значение переменной byteNumber с типом byte равно " + byteNumber);
+        System.out.println ("Значение переменной shortNumber с типом short равно " + shortNumber);
+        System.out.println ("Значение переменной longNumber с типом long равно " + longNumber);
+        System.out.println ("Значение переменной floatNumber с типом float равно " + floatNumber);
+        System.out.println ("Значение переменной doubleNumber с типом double равно " + doubleNumber);
+
     }
 }
